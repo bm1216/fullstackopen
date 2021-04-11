3df83a3b2320191857a93283400b0115659f1391
Barun Mishra <mishrabarun13@gmail.com>
2021-04-11 17:39:03 +0100

    Finish exercise 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-const Header = ({course}) => <><h1>{course}</h1></>
+const Header = ({title}) => <><h1>{title}</h1></>
 
 const Content = ({parts}) => {
   return(
@@ -29,29 +29,33 @@ const Total = ({parts}) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={parts}/>
-      <Total parts={parts.map(x => x.exercises)}/>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts.map(x => x.exercises)}/>  
     </div>
   )
 }
+
+// Trying to abstract away as much as possible
 
 export default App
