f5480e3959d3efab483e62c8b16abc271116fb2c
Barun Mishra <mishrab@Baruns-MacBook-Pro.local>
2021-04-27 15:43:10 +0100

    complete exercise 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addToPhonebook = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  console.log(persons);
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addToPhonebook}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => 
+          <li key={person.name}>{person.name}</li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
+
