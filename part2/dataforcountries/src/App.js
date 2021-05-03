165e0ee313fcd7d2f51cfcd172837d8ccdb3db33
Barun Mishra <mishrab@Baruns-MacBook-Pro.local>
2021-05-03 16:57:50 +0100

    Complete Exercise 2.13

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -1,38 +1,58 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Country = ({country}) => {
-  return (
-    <div>
-      <h2>{country.name}</h2>
-      <p>capital {country.capital}</p>
-      <p>population {country.population}</p>
-      <h3>languages</h3>
-      <ul>
-        {country.languages
-          .map((language, id) => {
-            return <li key={id}>{language.name}</li>
-          })}
-      </ul>
-      <img src={country.flag} alt="Not available"/>
-    </div>
-   
-  )
+const Country = ({country, show}) => {
+  if (show) {
+    return (
+      <div>
+        <h2>{country.name}</h2>
+        <p>capital {country.capital}</p>
+        <p>population {country.population}</p>
+        <h3>languages</h3>
+        <ul>
+          {country.languages
+            .map((language, id) => {
+              return <li key={id}>{language.name}</li>
+            })}
+        </ul>
+        <img src={country.flag} alt="Not available"/>
+      </div>
+    )
+  } else {
+    return (
+      null
+    )
+  }
 }
 
 const Countries = ({countries}) => {
+
+  const [showList, setShowList] = useState(new Array(countries.length).fill(false))
+
+  const handleShow = (id) => {
+    console.log(id);
+    let temp = [...showList]
+    temp[id] = !temp[id]
+    setShowList(temp)
+  }
+
   if (countries.length === 0) {
     return (
       null
     )
   } else if (countries.length === 1) {
     return (
-      <Country country={countries[0]}/>
+      <Country country={countries[0]} show={true}/>
     )
   } else if (1 < countries.length && countries.length < 10) {
     return (
-      countries.map(country => {
-        return <p key={country.numericCode}>{country.name}</p>
+      countries.map((country, id) => {
+        return (
+          <div key={country.numericCode}>
+            {country.name} <button onClick={() => handleShow(id)}>{showList[id] ? "hide" : "show"}</button>
+            <Country country={country} show={showList[id]}/>
+          </div>
+        )       
       })
     )
   } else {
