0681acb90d8b2bde432e9520d298fcc1b398d2c9
Barun Mishra <mishrab@Baruns-MacBook-Pro.local>
2021-05-13 15:07:31 +0100

    Gone through section e of part 2

--- practice/part2/src/App.js
+++ practice/part2/src/App.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
+import Notification from './components/Notification'
+import Footer from './components/Footer'
 
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     console.log('effect')
@@ -54,9 +57,13 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(
-          `the note '${note.content}' was already deleted from the server`
+        setErrorMessage(
+          `Note '${note.content}' was already deleted from the server`
         )
+
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         // Remove the already deleted note from the local state
         setNotes(notes.filter(n => n.id !== id))
       })
@@ -73,6 +80,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage}/>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -87,6 +95,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange}/>
         <button type="submit">save</button>
       </form>
+      <Footer/>
     </div>
   )
 }
